d88dbdbbc669ef63ee997bfb6305824272bb0a6c
JeffreyBahr <bahr.programmer@gmail.com>
2020-09-03 19:10:51 -0600

    adding employment and portfolio sections

---
+++ main.js
@@ -0,0 +1,20 @@
+// Smooth Scrolling
+$('#navbar a').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 65
+      }, 800);
+    }
+  });
+  
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 65) {
+      document.querySelector('#navbar').style.opacity = 0.8;
+    } else {
+      document.querySelector('#navbar').style.opacity = 1;
+    }
+  });
